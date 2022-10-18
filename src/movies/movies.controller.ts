c347d9cde33a64444ea926483c872ff26b8f1766
kj-dev <agassi77.dev@gmail.com>
2022-10-18 14:56:13 +0900

    DTO and Validation

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,8 +6,9 @@ import {
   Param,
   Patch,
   Post,
-  Query,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -26,7 +27,7 @@ export class MoviesController {
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
@@ -36,7 +37,7 @@ export class MoviesController {
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData);
   }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,12 +12,12 @@ export class MoviesService {
   }
 
   getOne(id: number): Movie {
-    const movie = this.movies.find((movie) => movie.id === +id);
+    const movie = this.movies.find((movie) => movie.id === id);
     if (!movie) throw new NotFoundException(`Movie not found: ${id}`);
     return movie;
   }
 
-  create(movieData: Movie) {
+  create(movieData: CreateMovieDto) {
     this.movies.push({
       id: this.movies.length + 1,
       ...movieData,
@@ -24,14 +26,13 @@ export class MoviesService {
 
   deleteOne(id: number): boolean {
     this.getOne(id);
-    this.movies = this.movies.filter((movie) => movie.id !== +id);
+    this.movies = this.movies.filter((movie) => movie.id !== id);
     return true;
   }
 
-  update(id: number, movieData: Movie) {
+  update(id: number, updateData: UpdateMovieDto) {
     this.getOne(id);
     this.movies = this.movies.map((movie) => {
-      if (movie.id === +id) return { ...movie, ...movieData };
       return movie.id === id ? { ...movie, ...updateData } : movie;
     });
   }
